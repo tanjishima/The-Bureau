d8d440bdb2b14103f64ce2898fd9e47fd1cc771d
Tanjila Shima <alaminrony4168@gmail.com>
2024-06-09 20:31:59 +0600

    add-text

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+       fontFamily:{
+        Grotesk:["Schibsted"],
+        lora:["Lora"],
+       },
+    },
+  },
+  plugins: [],
+}
+
